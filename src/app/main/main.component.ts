0c99c684c1ab7c56da6e1b7b1a79da3c05aaa2c8
HannaKogut <hanna.pol94@gmail.com>
2018-01-18 19:03:22 +0200

    ajkfs

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -12,52 +12,53 @@ import {
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
   animations: [
-  	trigger('scrollLeft', [
-  		state('show', style({
-  		})),
-  		state('hide', style({
-  			opacity: 0,
-  			transform: "translateX(-100%)"
-  		})),
-  		transition('show => hide', animate('1000ms ease-out')),
-  		transition('hide => show', animate('1000ms ease-in'))
-  		]),
-  	trigger('scrollRight', [
-  		state('show', style({
-  			opacity: 1,
-  			transform: "translateX(0)"
-  		})),
-  		state('hide', style({
-  			opacity: 0,
-  			transform: "translateX(100%)"
-  		})),
-  		transition('show => hide', animate('3000ms ease-out')),
-  		transition('hide => show', animate('1000ms ease-in'))
-  		])
+    trigger('scrollLeft', [
+      state('show', style({
+      })),
+      state('hide', style({
+        opacity: 0,
+        transform: "translateX(-100%)"
+      })),
+      transition('show => hide', animate('1000ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in'))
+      ]),
+    trigger('scrollRight', [
+      state('on', style({
+        opacity: 1,
+        transform: "translateX(0)"
+      })),
+      state('off', style({
+        opacity: 0,
+        transform: "translateX(100%)"
+      })),
+      transition('on => off', animate('3000ms ease-out')),
+      transition('off => on', animate('1000ms ease-in'))
+      ])
   ]
 })
 export class MainComponent implements OnInit {
 
-	state = 'hide';
+  state = 'hide';
+  toggle = 'off';
 
 
   constructor(public el: ElementRef) { }
 
 
+
   @HostListener('window:scroll', ['$event'])
-  	checkScroll() {
-  		const componentPosition = this.el.nativeElement.offsetTop
-  		const scrollPosition = window.pageYOffset
+    checkScroll() {
+      const componentPosition = this.el.nativeElement.offsetTop
+      const scrollPosition = window.pageYOffset
 
+      if (scrollPosition > componentPosition) {
+        this.state = 'show'
+      }
 
-  		if (scrollPosition >= componentPosition) {
-  			this.state = 'show'
-  		} 
-  	}  
-  		ngOnInit() {
+      if(scrollPosition > 1500){
+        this.toggle = 'on'
+      }
+    }  
+      ngOnInit() {
   }
-  	}
-
-
-
-
+    }
